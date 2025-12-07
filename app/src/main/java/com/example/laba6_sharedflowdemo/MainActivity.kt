f180d52c748228beeaa0493123a7fc0de3946d38
aaaaapchy <lidija.verner@yandex.ru>
2025-12-07 23:02:27 +0700

    Сбор данных о потоке

--- app/src/main/java/com/example/laba6_sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/example/laba6_sharedflowdemo/MainActivity.kt
@@ -6,13 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.laba6_sharedflowdemo.ui.theme.Laba6_SharedFlowDemoTheme
+import kotlinx.coroutines.flow.SharedFlow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +29,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Laba6_SharedFlowDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(modifier = Modifier)
+                    ScreenSetup(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -29,9 +39,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ScreenSetup(modifier: Modifier = Modifier, viewModel: DemoViewModel = viewModel()) {
-    MainScreen(modifier)
+fun ScreenSetup(
+    modifier: Modifier = Modifier,
+    viewModel: DemoViewModel = viewModel()
+) {
+    MainScreen(
+        modifier = modifier,
+        sharedFlow = viewModel.sharedFlow
+    )
 }
+
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    sharedFlow: SharedFlow<Int>
+) {
+    val messages = remember { mutableStateListOf<Int>() }
+
+    LaunchedEffect(key1 = Unit) {
+        sharedFlow.collect { value ->
+            messages.add(value)
+        }
+    }
+
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        items(messages) { item ->
+            Text(
+                text = "Collected Value = $item",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laba6_SharedFlowDemoTheme {
+        ScreenSetup()
+    }
 }
